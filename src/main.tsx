a421b33bdd9786560fa2dec6b97940e738955628
negoro26 <negrescualex05@gmail.com>
2026-01-12 17:47:41 +0200

    Fix base path and personalize portfolio

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-    <BrowserRouter basename="/negoro26.github.io/">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
     </BrowserRouter>
 );
